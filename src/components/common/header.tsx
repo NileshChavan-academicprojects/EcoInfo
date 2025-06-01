9230047844c052b49fc8696269b23630156d63fd
Nilesh Chavan <cnilesh123.nc@gmail.com>
2025-06-01 10:52:35 +0000

    Add Man logo on profile

--- src/components/common/header.tsx
+++ src/components/common/header.tsx
@@ -58,7 +58,7 @@ export function Header() {
           <DropdownMenuTrigger asChild>
             <Button variant="ghost" size="icon" className="rounded-full">
               <Avatar className="h-8 w-8">
-                <AvatarImage src="https://placehold.co/100x100.png" alt="User avatar" data-ai-hint="user avatar person" />
+                <AvatarImage src="https://placehold.co/100x100.png" alt="User avatar" data-ai-hint="man user avatar" />
                 <AvatarFallback>U</AvatarFallback>
               </Avatar>
             </Button>
